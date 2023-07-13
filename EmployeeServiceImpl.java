15dd98bf6c77cb17f3bae289d4e15de8de19020a
vikrampurohit15 <139479132+vikrampurohit15@users.noreply.github.com>
2023-07-13 20:40:00 +0530

    Create EmployeeServiceImpl.java

---
+++ EmployeeServiceImpl.java
@@ -0,0 +1,23 @@
+package net.javaguides.springboot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Override
+	public List<Employee> getAllEmployees() {
+		System.out.println("employeeRepository.findAll()="+employeeRepository.findAll().size());
+		System.out.println("employeeRepository="+employeeRepository);
+		return employeeRepository.findAll();
+	}
+
+}

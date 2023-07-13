53424291e3e77c82d0591e62ccd93515f9c8d919
vikrampurohit15 <139479132+vikrampurohit15@users.noreply.github.com>
2023-07-13 20:38:29 +0530

    Create EmployeeService.java

---
+++ EmployeeService.java
@@ -0,0 +1,11 @@
+package net.javaguides.springboot.service;
+
+import java.util.List;
+
+import net.javaguides.springboot.model.Employee;
+
+public interface EmployeeService {
+	
+	List<Employee> getAllEmployees();
+
+}
